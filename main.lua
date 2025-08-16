75a120d5358c86ecf245179880ad31e40075114d
Juniper <pgiemigbvm>
2025-08-16 10:38:21 -0500

    bla

---
+++ main.lua
@@ -0,0 +1,41 @@
+local values = {
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+}
+local function exec_turn(curr_score)
+    
+end
+local function start_game()
+
+
+
+end
+local function main_menu()
+    print('Welcome to Teal Blackjack!')
+    print('Please select an option to play:')
+    print('[P] Play')
+    print('[Q] Quit')
+    local input = io.read()
+    if input == 'P' then
+        start_game()
+    end
+    
+end
+
+
+
+
+local function restart()
+    
+    
+end
+
+main_menu()
